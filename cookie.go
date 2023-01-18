073d57c6a5e9d26071887d62650ab90be0926945
littlefish12345 <little_fish@littlefish.moe>
2023-01-18 13:17:39 +0800

    兼容nginx转发后的cookie header

--- cookie.go
+++ cookie.go
@@ -7,15 +7,20 @@ import (
 
 func (request *Request) DecodeCookie() map[string]string {
 	cookieMap := make(map[string]string)
-	if cookie, ok := request.Header["Cookie"]; ok {
-		cookieList := strings.Split(cookie, "; ")
-		var cookieFirst string
-		for i := 0; i < len(cookieList); i++ {
-			singleCookieList := strings.Split(cookieList[i], "=")
-			if len(singleCookieList) == 2 {
-				cookieFirst, _ = url.QueryUnescape(singleCookieList[0])
-				cookieMap[cookieFirst], _ = url.QueryUnescape(singleCookieList[1])
-			}
+	var cookieHeader string
+	var ok bool
+	if cookieHeader, ok = request.Header["Cookie"]; !ok {
+		if cookieHeader, ok = request.Header["cookie"]; !ok {
+			return cookieMap
+		}
+	}
+	cookieList := strings.Split(cookieHeader, "; ")
+	var cookieFirst string
+	for i := 0; i < len(cookieList); i++ {
+		singleCookieList := strings.Split(cookieList[i], "=")
+		if len(singleCookieList) == 2 {
+			cookieFirst, _ = url.QueryUnescape(singleCookieList[0])
+			cookieMap[cookieFirst], _ = url.QueryUnescape(singleCookieList[1])
 		}
 	}
 	return cookieMap
